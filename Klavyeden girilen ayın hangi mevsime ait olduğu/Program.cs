449a974045ec281caab5d5431bbd11be51635d03
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:59:02 +0300

    Add project files.

---
+++ Klavyeden girilen ayın hangi mevsime ait olduğu/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_girilen_ayın_hangi_mevsime_ait_olduğu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ayı girin");
+            string ay = Console.ReadLine();
+
+            switch (ay)
+            {
+                case "ocak": Console.WriteLine("Kış"); break;
+                case "şubat": Console.WriteLine("Kış"); break;
+                case "mart": Console.WriteLine("İlkbahar"); break;
+                case "nisan": Console.WriteLine("İlkbahar"); break;
+                case "mayıs": Console.WriteLine("İlkbahar"); break;
+                case "haziran": Console.WriteLine("Yaz"); break;
+                case "temmuz": Console.WriteLine("Yaz"); break;
+                case "ağustos": Console.WriteLine("Yaz"); break;
+                case "eylül": Console.WriteLine("Sonbahar"); break;
+                case "ekim": Console.WriteLine("Sonbahar"); break;
+                case "kasım": Console.WriteLine("Sonbahar"); break;
+                case "aralık": Console.WriteLine("Kış"); break;
+                default:Console.WriteLine("Böyle bir ay yok");break;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
